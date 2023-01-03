8b97d077c5c9f3c5a70412fc4fd75175821c51a0
tamimhasan19702 <tamimhasancu@gmail.com>
2023-01-04 00:48:17 +0600

    app js updates

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 import './App.css';
 
 function App() {
+
+  const activeMenu = true;
+
+
   return (
     <div>
       <BrowserRouter> 
@@ -23,6 +27,54 @@ function App() {
         </TooltipComponent>
 
       </div>
+      {
+       activeMenu ? (
+        <div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white'>
+          sidebar
+        </div>
+       ) : (
+        <div className='w-0 dark:bg-secondary-dark-bg'>
+          sidebar
+        </div>
+       )}
+       <div className={ 
+        `dark:bg-main-bg bg-main-bg min-h-screen md:ml-72 w-full ${activeMenu ? 'md:ml-72' : 'flex-2'}`
+        }>
+         <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
+          Navbar
+         </div>
+       </div>
+
+        <div>
+          <Routes>
+            {/* dashboard */}
+            <Route path='/' element='ECommerce'/>
+            <Route path='/ecommerce' element='ECommerce'/>
+
+            {/* pages */}
+            <Route path='/orders' element="Orders"/>
+            <Route path='/employees' element="Employees"/>
+            <Route path='/customers' element="Customers"/>
+
+            {/* Apps */}
+            <Route path='/Kanban' element="Kanban"/>
+            <Route path='/editor' element="Editor"/>
+            <Route path='/calender' element="Calender"/>
+            <Route path='/color-picker' element="ColorPicker"/>
+
+            {/* Charts */}
+            <Route path='/line' element='Line'/>
+            <Route path='/area' element='Area'/>
+            <Route path='/bar' element='Bar'/>
+            <Route path='/pie' element='Pie'/>
+            <Route path='/financial' element='Financial'/>
+            <Route path='/color-mapping' element='ColorMapping'/>
+            <Route path='/pyramid' element='Pyramid'/>
+            <Route path='/stacked' element='Stacked'/>
+
+          </Routes>
+        </div>
+
 
       </div>
 
