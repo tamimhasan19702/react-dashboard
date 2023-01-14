0406c02f59ab5c3081922e9c97443950c37254ca
tamimhasan19702 <tamimhasancu@gmail.com>
2023-01-14 15:25:57 +0600

    Homapage styling updates

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ function App() {
          <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
           <Navbar />
          </div>
-       </div>
+       
 
         <div>
           <Routes>
@@ -81,7 +81,7 @@ function App() {
 
           </Routes>
         </div>
-
+        </div>
 
       </div>
 
