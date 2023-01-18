7676cb53940339f8af7d43ec29e3c4d78b8ec8fe
tamimhasan19702 <tamimhasancu@gmail.com>
2023-01-18 15:47:15 +0600

    orders data images and information rendered

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -6,8 +6,22 @@ import { Header } from '../components';
 
 const Orders = () => {
   return (
-    <div>
+    <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'> 
+      
       <Header category="Page" title="Orders"/>
+      <GridComponent
+      id='gridcomp'
+      dataSource={ordersData}
+      >
+        <ColumnsDirective>
+        {
+          ordersGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))
+        }
+        </ColumnsDirective>
+      </GridComponent>
+
     </div>
   )
 }
