78a0b09cf6b5e3990083a7a75dcf7bb75eb04073
tamimhasan19702 <tamimhasancu@gmail.com>
2023-01-23 01:11:09 +0600

    theme settings styles improved

--- src/components/ThemeSettings.jsx
+++ src/components/ThemeSettings.jsx
@@ -36,8 +36,8 @@ const ThemeSettings = () => {
               name="theme"
               value='Light'
               className='cursor-pointer'
-              onChange={() => {}}
-              checked={true}
+              onChange={setMode}
+              checked={currentMode === 'Light'}
               />
               <label htmlFor="light" className='ml-2 text-md cursor-pointer'>Light</label>
              </div>
@@ -49,8 +49,8 @@ const ThemeSettings = () => {
               name="theme"
               value='Dark'
               className='cursor-pointer'
-              onChange={() => {}}
-              checked={true}
+              onChange={setMode}
+              checked={currentMode === 'Dark'}
               />
               <label htmlFor="dark" className='ml-2 text-md cursor-pointer'>Dark</label>
              </div>
@@ -71,9 +71,9 @@ const ThemeSettings = () => {
                     type='button'
                     className='h-10 w-10 rounded-full cursor-pointer'
                     style={{backgroundColor: item.color}}
-                    onClick={() => {}}
+                    onClick={() => setColor(item.color)}
                     >
-                      <BsCheck className={`ml-2 text-2xl text-white ${false ? 'block' : 'hidden' }`}/>
+                      <BsCheck className={`ml-2 text-2xl text-white ${item.color === currentColor ? 'block' : 'hidden' }`}/>
                     </button>
                   </div>     
                   </TooltipComponent>
